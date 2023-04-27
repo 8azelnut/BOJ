6351ed458c8e29df61fb8d676308ca76328f5b40
woody0808 <kookb20000@naver.com>
2023-04-27 16:30:21 +0900

    치킨댄스를 추는 곰곰이를 본 임스

---
+++ Bronze_4/25191.py
@@ -0,0 +1,9 @@
+chicken_cnt_all = int(input())
+coke, beer = map(int, input().split())
+
+beverage = coke // 2 + beer
+
+if beverages <= chicken_cnt_all:
+    print(beverages)
+else:
+    print(chicken_cnt_all)
