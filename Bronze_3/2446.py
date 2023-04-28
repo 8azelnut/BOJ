c72b8cf39e5836a4d426ca09c92688ad6399b265
woody0808 <kookb20000@naver.com>
2023-04-28 16:21:17 +0900

    별 찍기 - 9

---
+++ Bronze_3/2446.py
@@ -0,0 +1,9 @@
+num = int(input())
+
+for i in range(num, 1, -1):
+    print(" " * (num - i) + "*" * i + "*" * (i - 1))
+
+print(end="")
+
+for i in range(1, num+1):
+    print(" " * (num - i) + "*" * i + "*" * (i - 1))
