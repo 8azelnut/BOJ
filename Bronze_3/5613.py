a6da5192d05c660340c1abfe64be9f468d92a2a3
woody0808 <kookb20000@naver.com>
2023-05-02 13:57:35 +0900

    계산기 프로그램

---
+++ Bronze_3/5613.py
@@ -0,0 +1,20 @@
+res = int(input())
+
+while True:
+    op = input()
+
+    if op == "=":
+        break
+
+    n = int(input())
+
+    if op == "+":
+        res += n
+    elif op == "-":
+        res -= n
+    elif op == "*":
+        res *= n
+    elif op == "/":
+        res //= n
+
+print(res)
