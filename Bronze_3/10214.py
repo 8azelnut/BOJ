00f71cbe511ae65f4ea86307325195a682db554c
woody0808 <kookb20000@naver.com>
2023-05-03 16:42:31 +0900

    Baseball

---
+++ Bronze_3/10214.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for i in range(t):
+    y_score = k_score = 0
+
+    for j in range(9):
+        y, k = map(int, input().split())
+
+        y_score += y
+        k_score += k
+
+    if y_score > k_score:
+        print("Yonsei")
+    elif y_score < k_score:
+        print("Korea")
+    else:
+        print("Draw")
