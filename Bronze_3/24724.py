e827ec8cee51cfa9f9800ae92e547bcbe96fa57a
woody0808 <kookb20000@naver.com>
2023-05-06 18:33:06 +0900

    현대모비스와 함께하는 부품 관리

---
+++ Bronze_3/24724.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for i in range(1, t+1):
+    n = int(input())
+    a, b = map(int, input().split())
+
+    for j in range(n):
+        u, v = map(int, input().split())
+
+    print(f"Material Management {i}")
+    print("Classification ---- End!")
