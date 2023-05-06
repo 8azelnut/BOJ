efd3a1fefdb1a7c3b2d7f641ddd37a73a2d506e1
woody0808 <kookb20000@naver.com>
2023-05-06 18:30:58 +0900

    골뱅이 찍기 - ㅂ

---
+++ Bronze_3/23808.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(n*2):
+    print("@" * n + " " * n * 3 + "@" * n)
+
+for i in range(n):
+    print("@" * n * 5)
+
+for i in range(n):
+    print("@" * n + " " * n * 3 + "@" * n)
+
+for i in range(n):
+    print("@" * n * 5)
