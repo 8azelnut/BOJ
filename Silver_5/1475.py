cf3788f832fcddfb58b4606e8d572d69589c3f53
woody0808 <kookb20000@naver.com>
2023-05-20 15:39:44 +0900

    방 번호

---
+++ Silver_5/1475.py
@@ -0,0 +1,16 @@
+n = input()
+
+li = [0 for i in range(10)]
+
+for i in range(len(n)):
+    num = int(n[i])
+
+    if num == 6 or num == 9:
+        if li[6] <= li[9]:
+            li[6] += 1
+        else:
+            li[9] += 1
+    else:
+        li[num] += 1
+
+print(max(li))
